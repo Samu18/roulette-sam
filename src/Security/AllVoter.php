5d51b647b04fa01d993d88fc7ea9d92e92a702ff
Samu18 <samuel.sanchez@correounivalle.edu.co>
2022-05-14 03:22:10 -0500

    Update AllVoter.php

--- src/Security/AllVoter.php
+++ src/Security/AllVoter.php
@@ -1,73 +1,38 @@
 <?php
 
-namespace App\Security;
+namespace App\EventListener;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class AllVoter extends Voter
+class SecurityControl
 {
-    /**
-     * AllVoter constructor.
-     */
 
-    private $em;
+    protected $auth;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(AuthorizationChecker $auth)
     {
-        $this->em = $em;
+        $this->auth = $auth;
     }
 
-
-    /**
-     * Determines if the attribute and subject are supported by this voter.
-     *
-     * @param string $attribute An attribute
-     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
-     *
-     * @return bool True if the attribute and subject are supported, false otherwise
-     */
-    protected function supports($attribute, $subject)
+    public function checkPermissionUser(GetResponseEvent $event)
     {
-        return true;
-    }
-
-    /**
-     * Perform a single access check operation on a given attribute, subject and token.
-     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
-     *
-     * @param string $attribute
-     * @param mixed $subject
-     * @param TokenInterface $token
-     *
-     * @return bool
-     */
-    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
-    {
-        $user = $token->getUser();
-
-        if (!$user instanceof \App\Entity\User) {
-            return false;
-        }
-
-        /*Consulta para obtener un registro desde los roles o grants para ver si el usuario tiene acceso o no*/
-        try {
-
-            $permissions = strpos($user->getPermissions(), "|" . $attribute . "|");
-            $permissionsPublic = "|dashboard|";
-            $permissionsPublic = strpos($permissionsPublic, "|" . $attribute . "|");
-
-            if ($permissionsPublic === false && $permissions === false  && $user->getPermissions() != "x") {
-                return false;
-            } else {
-                return true;
+        $routeName = $event->getRequest()->attributes->get('_route');
+        //Se exceptuan las rutas que tengan que ver con fos (Friends of symfony), porque esto tiene que ver con el login, registro, recuperación de contraseñas...
+        //También lo que tiene que ver con el profiler (Barra de desarrollo) y cosas del sistema como twig y wdt. También las que están permitidas por
+        //los access control
+        if ($routeName != null
+            && stripos($routeName, '_anonymous') === false
+            && stripos($routeName, '_twig_') === false
+            && stripos($routeName, '_profiler') === false
+            && stripos($routeName, 'roulette_') === false
+            && stripos($routeName, '_wdt') === false) {
+            if (!$this->auth->isGranted($routeName)) {
+                $exception = new AccessDeniedException('Access denied');
+                $exception->setAttributes($routeName);
+                throw $exception;
             }
-
-        } catch (\Exception $ex) {
-            dump('An error occurred checking grants of the user');
-            die();
         }
-
     }
 }
